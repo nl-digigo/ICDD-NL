284d25515eedaf98b861a183082191158923d2e7
Niels Hoffmann <niels.hoffmann@hotmail.com>
2021-03-12 16:01:11 +0100

    update naar respec nl template

---
+++ config-old.js
@@ -0,0 +1,57 @@
+
+var respecConfig =
+{
+  specStatus: "WV",                // Werk Versie
+  specType: "HR",                   // HandReiking
+   format: "markdown",                 // altijd "markdown"
+   github: "https://github.com/bimloket/COINS-3.0-ICDD",
+  editors:
+  [
+    {
+      name:       "Elisabeth Klören",
+      company:    "CROW",
+      companyURL: "https://www.crow.nl"
+    },
+    {
+      name:       "Niels Hoffmann",
+      company:    "Provincie Noord-Holland",
+      companyURL: "https://www.noord-holland.nl"
+    },
+  ],
+  authors:
+  [
+    {
+      name:       "Niels Reyngoud",
+      company:    "Provincie Gelderland",
+      companyURL: "https://www.gelderland.nl"
+    },
+    {
+      name:       "Bart Bink",
+      company:    "Braindex",
+      companyURL: "http://www.braindex.nl"
+    },
+    {
+      name:       "Michel Böhms",
+      company:    "TNO",
+      companyURL: "https://www.tno.nl"
+    },
+    {
+      name:       "Benno Koehorst",
+      company:    "Rijkswaterstaat",
+      companyURL: "https://www.rws.nl"
+    },
+    {
+      name:       "Rob van de Werken",
+      company:    "Rijkswaterstaat",
+      companyURL: "https://www.rws.nl"
+    },
+    
+  ],
+   shortName: "coins3",
+  license: "cc-by",              //-- bronvermelding, geen afgeleide werken (default)
+   localBiblio:
+  {
+   
+  }
+
+};
